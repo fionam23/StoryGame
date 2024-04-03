fbe1f6460e4a2e920cb49e19df2663189824badd
fiona <fionam23@nycstudents.net>
2024-04-03 13:51:08 -0400

    -Trying to create window

---
+++ src/MainFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame implements Runnable{
+    private Thread windowThread
+    private Thread windowThread;
+    public MainFrame(String display) {
+        super(display);
+        int frameWidth;
+        int frameHeight;
+        panel = new DrawPanel();
+    }
+    @Override
+    public void run() {
+        while (true) {
+
+        }
+    }
+}
