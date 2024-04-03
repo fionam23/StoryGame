f11cdc6f819d1d1b833dfb9a133e1bce6f204457
fionam23 <144394184+fionam23@users.noreply.github.com>
2024-04-03 13:57:27 -0400

    Update MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,13 +1,17 @@
 import javax.swing.*;
 
 public class MainFrame extends JFrame implements Runnable{
-    private Thread windowThread
     private Thread windowThread;
+    private Dr
     public MainFrame(String display) {
         super(display);
         int frameWidth;
         int frameHeight;
         panel = new DrawPanel();
+    }
+    public void startThread(){
+        windowThread = new Thread(this);
+        windowThread.start();
     }
     @Override
     public void run() {
